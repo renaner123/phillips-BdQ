0730bfef3b49695112adf4925a9fee81a3b2f01a
Renan <renaner123@gmail.com>
2023-01-30 20:18:50 -0300

    iniciando repositório

---
+++ src/main/java/com/phillips/saper/bancoquestoes/BancoQuestoesApplication.java
@@ -0,0 +1,13 @@
+package com.phillips.saper.bancoquestoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoQuestoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoQuestoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phillips/saper/bancoquestoes/BancoQuestoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phillips.saper.bancoquestoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoQuestoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
