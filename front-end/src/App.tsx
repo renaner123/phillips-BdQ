cd5e763458b2bd10d637617954336797e64ea823
Renan <renaner123@gmail.com>
2023-03-22 20:55:46 -0300

    enviando requisição com header authentication

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,226 +1,29 @@
-import * as React from "react";
+import './App.css';
+import { BrowserRouter, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './StyleSheet/index.scss';
+import NavbarComponent from './Components/Navbar';
+import Home from './Pages/Home';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
 import FileList from './Pages/FileList';
-import {
-  Routes,
-  Route,
-  Link,
-  useNavigate,
-  useLocation,
-  Navigate,
-  Outlet,
-} from "react-router-dom";
-import { fakeAuthProvider } from "./context/Auth";
-import axios from "axios";
 
-export default function App() {
-  return (
-    <AuthProvider>
-      <h1>Auth Example</h1>
 
-      <p>
-        This example demonstrates a simple login flow with three pages: a public
-        page, a protected page, and a login page. In order to see the protected
-        page, you must first login. Pretty standard stuff.
-      </p>
 
-      <p>
-        First, visit the public page. Then, visit the protected page. You're not
-        yet logged in, so you are redirected to the login page. After you login,
-        you are redirected back to the protected page.
-      </p>
-
-      <p>
-        Notice the URL change each time. If you click the back button at this
-        point, would you expect to go back to the login page? No! You're already
-        logged in. Try it out, and you'll see you go back to the page you
-        visited just *before* logging in, the public page.
-      </p>
-
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<PublicPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          
-          <Route
-            path="/download"
-            element={
-              <RequireAuth>
-                <FileList />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/protected"
-            element={
-              <RequireAuth>
-                <ProtectedPage />
-              </RequireAuth>
-            }
-          />
-        </Route>
-      </Routes>
-    </AuthProvider>
-  );
-}
-
-function Layout() {
+function App() {
   return (
     <div>
-      <AuthStatus />
-
-      <ul>
-        <li>
-          <Link to="/">Public Page</Link>
-        </li>
-        <li>
-          <Link to="/download">Protected Page</Link>
-        </li>
-      </ul>
-
-      <Outlet />
+      <NavbarComponent/>
+      <BrowserRouter>
+            <Routes>
+              <Route path="/home" element= { <Home/>} />
+              <Route path="/register" element= { <Register/>} />
+              <Route path="/" element= { <Login/>} />
+              <Route path="/download" element= { <FileList/>} />
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
 
-interface AuthContextType {
-  user: any;
-  signin: (user: string, password: string,callback: VoidFunction) => void;
-  signout: (callback: VoidFunction) => void;
-}
-
-let AuthContext = React.createContext<AuthContextType>(null!);
-
-function AuthProvider({ children }: { children: React.ReactNode }) {
-  let [user, setUser] = React.useState<any>(null);
-  let [password, setPassword] = React.useState<any>(null);
-
-  let signin = async (newUser: string, newPassword: string, callback: VoidFunction) => {
-
-    try {
-      await axios.post("http://127.0.0.1:8080/auth/authenticate", {
-        username: newUser,
-        password: newPassword,
-      }).then((res) => {
-        console.log(res)
-        if (res.status === 200) {
-          return fakeAuthProvider.signin(() => {
-            setUser(newUser);
-            setPassword(newPassword);
-            callback();
-          });
-        }
-        else {
-          alert("Tratar erro");
-        }
-      }, fail => {
-        console.error(fail); // Error!
-      });
-    }
-    catch (err) {
-      alert(err);
-    }
-  };
-
-  let signout = (callback: VoidFunction) => {
-    return fakeAuthProvider.signout(() => {
-      setUser(null);
-      callback();
-    });
-  };
-
-  let value = { user, password, signin, signout };
-
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-}
-
-function useAuth() {
-  return React.useContext(AuthContext);
-}
-
-function AuthStatus() {
-  let auth = useAuth();
-  let navigate = useNavigate();
-
-  if (!auth.user) {
-    return <p>You are not logged in.</p>;
-  }
-
-  return (
-    <p>
-      Welcome {auth.user}!{" "}
-      <button
-        onClick={() => {
-          auth.signout(() => navigate("/"));
-        }}
-      >
-        Sign out
-      </button>
-    </p>
-  );
-}
-
-function RequireAuth({ children }: { children: JSX.Element }) {
-  let auth = useAuth();
-  let location = useLocation();
-
-  if (!auth.user) {
-    // Redirect them to the /login page, but save the current location they were
-    // trying to go to when they were redirected. This allows us to send them
-    // along to that page after they login, which is a nicer user experience
-    // than dropping them off on the home page.
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
-
-  return children;
-}
-
-function LoginPage() {
-  let navigate = useNavigate();
-  let location = useLocation();
-  let auth = useAuth();
-
-  let from = location.state?.from?.pathname || "/";
-
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-
-    let formData = new FormData(event.currentTarget);
-    let username = formData.get("username") as string;
-    let password = formData.get("password") as string;
-
-
-    auth.signin(username,password, () => {
-      // Send them back to the page they tried to visit when they were
-      // redirected to the login page. Use { replace: true } so we don't create
-      // another entry in the history stack for the login page.  This means that
-      // when they get to the protected page and click the back button, they
-      // won't end up back on the login page, which is also really nice for the
-      // user experience.
-      navigate(from, { replace: true });
-    });
-  }
-
-  return (
-    <div>
-      <p>You must log in to view the page at {from}</p>
-
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username: <input name="username" type="text" />
-        </label>{" "}
-        <label>
-          Password: <input name="password" type="text" />
-        </label>{" "}
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
-}
-
-function PublicPage() {
-  return <h3>Public</h3>;
-}
-
-function ProtectedPage() {
-  return <h3>Protected</h3>;
-}
+export default App;
